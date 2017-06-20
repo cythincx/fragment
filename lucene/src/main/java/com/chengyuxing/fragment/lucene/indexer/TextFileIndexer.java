5cc73d82685ce74bc5ce8de47d56adb81b504e32
cythincx <168chengyuxing@sina.com>
2017-06-20 23:44:27 +0800

    增加lucene模块

---
+++ lucene/src/main/java/com/chengyuxing/fragment/lucene/indexer/TextFileIndexer.java
@@ -0,0 +1,11 @@
+package com.chengyuxing.fragment.lucene.indexer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: chengyuxing
+ * Date: 2017/6/20
+ * Time: 下午11:44
+ * Description:
+ */
+public class TextFileIndexer {
+}
